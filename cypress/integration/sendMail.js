b638fa18ae70fa84d3d2382742bc2e34d079e78a
Natali Osnovenko <nataliosn3452@gmail.com>
2020-08-26 20:43:38 +0300

    send mail test

--- cypress/integration/sendMail.js
+++ cypress/integration/sendMail.js
@@ -1,33 +1,42 @@
+import HomePage from "../support/pageObjects/HomePage";
+
 const getTinymce = () => {
     return cy
         .get('iframe#composebody_ifr')
         .its('0.contentDocument.body')
         .then(cy.wrap)
 }
-const login = () => {
-    cy.visit("https://104.236.232.96/roundcube/");
-    cy.get('#rcmloginuser').type('qa-test@arixess.com');
-    cy.get('#rcmloginpwd').type('catch123');
-    cy.get('#rcmloginsubmit').click();
-    cy.wait(1000)
+
+beforeEach(() => new HomePage().login());
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
-beforeEach(login);
 
 describe('Check send email', () => {
 
-    it.skip('Email should be send', () => {
+    before(function () {
+        return cy.fixture('message.json').as('msgData')
+            .then(() => {
+                this.emailSubject = `${this.msgData.subject} ${uuidv4()}`
+            })
+
+    })
+
+    it('Email should be send', function () {
+
         cy.get('#rcmbtn107').click()
-        cy.get('#_to').type('nataliosn3452@gmail.com');
-        cy.get('#compose-subject').type('Cypress Test Task 1');
+        cy.get('#_to').type(this.msgData.to);
+        cy.get('#compose-subject').type(this.emailSubject);
         cy.get(':nth-child(2) > label > select').select('HTML').should('have.value', 'html');
         // cy.get('#rcmcomposepriority').select('High').should('have.value', '2');
         cy.get('#rcmcomposedsn').check().should('be.checked').and('have.value', '1');
 
-        const msgText = `Необходимо написать тест, который будет проверять работу функции отправки письма. Также, после успешной отправки письма, тест должен находить отправленное письмо в папке "Отправленные" и удалять его. `
-
         const tinymce = getTinymce();
-        tinymce.type(msgText);
-
+        tinymce.type(this.msgData.body);
         cy.fixture('img/test.png')
             .then(fileContent => {
                 cy.get('#compose-attachments')
@@ -41,11 +50,25 @@ describe('Check send email', () => {
         cy.wait(3000)
         cy.get('#rcmbtn107').click();
 
-
     });
 
-    it('Email will be send', () => {
+    it('Email will be send', function () {
         cy.get('#rcmliU2VudA > a').click();
+        cy.wait(1000)
+        cy.get('#quicksearchbox')
+            .type(this.emailSubject)
+            .should('have.value', this.emailSubject);
 
+        cy.get('form[name="rcmqsearchform"]').submit();
+        cy.wait(1000)
+
+        cy.get('#messagelist tbody tr').should('have.length', 1)
+
+        cy.get('#messagelist tbody tr')
+            .each(function ($element) {
+                cy.wrap($element).click()
+                cy.get('#rcmbtn111').click();
+                cy.wait(1000)
+            })
     })
 })
---
+++ cypress/support/pageObjects/HomePage.js
@@ -0,0 +1,10 @@
+export default class HomePage {
+
+    login() {
+        cy.visit("https://104.236.232.96/roundcube/");
+        cy.get('#rcmloginuser').type('qa-test@arixess.com');
+        cy.get('#rcmloginpwd').type('catch123');
+        cy.get('#rcmloginsubmit').click();
+        return cy.wait(1000)
+    }
+}
