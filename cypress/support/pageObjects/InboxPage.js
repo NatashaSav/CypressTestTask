4e106a1a1008adae426cd41a4e100209b6bb8984
Natali Osnovenko <nataliosn3452@gmail.com>
2020-08-27 00:22:01 +0300

    mark letter as read/unread test

--- cypress/support/pageObjects/InboxPage.js
+++ cypress/support/pageObjects/InboxPage.js
@@ -4,6 +4,10 @@ export default class InboxPage {
         cy.wait(1000);
     }
 
+    getLetters() {
+        return cy.get('#messagelist tbody tr')
+    }
+
     getFirstLetter() {
         return cy.get('#messagelist tbody tr').first()
     }
